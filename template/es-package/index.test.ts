61def415b7c11b8bb355e3000831421ad0fdfecb
forguo <wforguo@qq.com>
2024-12-06 15:49:10 +0800

    feat: 更新测试用例

--- template/es-package/index.test.ts
+++ template/es-package/index.test.ts
@@ -2,8 +2,8 @@ import { describe, expect, it } from 'vitest'
 import { {{ name }} } from '.'
 
 describe('{{ name }}', () => {
-    it('no param', () => {
+    it('test', () => {
         const test = {{ name }}()
-        expect(test).toEqual(undefined)
+        expect(test).not.toBe(undefined)
     })
 })
